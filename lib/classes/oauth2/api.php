842e640e298163f4baf103535eba5b0c4d263de4
Tse Kit Yam <tsekityam@users.noreply.github.com>
2018-01-24 14:31:36 +0800

    Add space between two words

--- lib/classes/oauth2/api.php
+++ lib/classes/oauth2/api.php
@@ -376,7 +376,7 @@ class api {
         }
 
         if (!$json = $curl->get($url)) {
-            $msg = 'Could not discover end points for identity issuer' . $issuer->get('name');
+            $msg = 'Could not discover end points for identity issuer ' . $issuer->get('name');
             throw new moodle_exception($msg);
         }
 
@@ -386,7 +386,7 @@ class api {
 
         $info = json_decode($json);
         if (empty($info)) {
-            $msg = 'Could not discover end points for identity issuer' . $issuer->get('name');
+            $msg = 'Could not discover end points for identity issuer ' . $issuer->get('name');
             throw new moodle_exception($msg);
         }
 
